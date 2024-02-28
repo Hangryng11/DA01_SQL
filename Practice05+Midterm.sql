ad835b1a8f81bb1e08672e68b845c9bdeb7acad5
Hangryng11 <160314544+Hangryng11@users.noreply.github.com>
2024-02-29 00:10:36 +0700

    Update Practice05+Midterm.sql

--- Practice05+Midterm.sql
+++ Practice05+Midterm.sql
@@ -4,3 +4,11 @@ from city,country
 where city.countrycode=country.code
 group by country.continent;
 -- Ex2
+SELECT 
+ROUND(COUNT(texts.email_id)::DECIMAL
+/COUNT(DISTINCT emails.email_id),2) AS activation_rate
+FROM emails
+LEFT JOIN texts
+  ON emails.email_id = texts.email_id
+  AND texts.signup_action = 'Confirmed';
+-- Ex3
